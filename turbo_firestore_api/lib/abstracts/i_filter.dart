9fdbd559b376fcada33031ca0fb76d154fe3dfdf
appboypov <info@codaveto.com>
2026-05-12 11:28:46 +0200

    feat: add IFilter interface and extension for enhanced filtering capabilities

--- turbo_firestore_api/lib/abstracts/i_filter.dart
+++ turbo_firestore_api/lib/abstracts/i_filter.dart
@@ -1,3 +1,11 @@
+import 'package:turbo_firestore_api/abstracts/t_model.dart';
+import 'package:turbo_firestore_api/models/t_filter.dart';
+import 'package:turbo_serializable/abstracts/t_writeable_id.dart';
+
 abstract interface class IFilter<MODEL> {
   bool filter<INPUT>({required MODEL model, INPUT? input});
+}
+
+extension IFilterExtension<DTO extends TWriteableId, MODEL extends TModel<DTO>> on IFilter<MODEL> {
+  TFilter<MODEL> asFilter<INPUT>([INPUT? input]) => TFilter(value: this, input: input);
 }
