035e36b2914e3c66d14906e3da503b318cfd3070
appboypov <info@codaveto.com>
2026-05-28 19:08:25 +0200

    refactor: enhance `TSerializableId` for extensibility and multi-format serialization
    Replaced `TWriteableCustomId` with `TWriteableId`, added support for YAML, Markdown, and XML builders with improved metadata handling and flexible serialization options.

--- turbo_serializable/lib/abstracts/t_serializable_id.dart
+++ turbo_serializable/lib/abstracts/t_serializable_id.dart
@@ -1,71 +1,119 @@
-import 'package:turbo_serializable/abstracts/t_writeable_custom_id.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:turbo_serializable/abstracts/t_writeable_id.dart';
+import 'package:turbo_serializable/constants/ts_defaults.dart';
+import 'package:turbo_serializable/extensions/ts_map_extension.dart';
+import 'package:turbo_serializable/markdown/factories/t_md_factory.dart';
+import 'package:turbo_serializable/typedefs/key_value_builder_def.dart';
 
-/// Abstract base class providing serialization for objects with identifiers.
+/// Abstract base class providing serialization to multiple data formats.
 ///
-/// Extends [TWriteableCustomId] and adds convenience methods for converting
-/// the object into YAML, Markdown, and XML representations. This class
-/// provides a non-generic version that works with dynamic ID types.
-///
-/// For type-safe ID handling, use [TSerializableCustomId] in
-/// `t_serializable_custom_id.dart`.
-///
-/// Example:
-/// ```dart
-/// class User extends TSerializableId {
-///   User({required this.name});
-///
-///   final String name;
-///
-///   @override
-///   String get id => 'user-123';
-///
-///   @override
-///   Map<String, dynamic> toJson() => {'name': name};
-/// }
-/// ```
-abstract class TSerializableId extends TWriteableCustomId {
+/// Extends [TWriteableId] and provides convenience methods for converting
+/// the object into YAML, Markdown, and XML representations.
+/// Requires concrete implementations to provide builder functions for
+/// each format they wish to support.
+abstract class TSerializableId extends TWriteableId {
+  TSerializableId({
+    this.yamlBuilder,
+    this.mdFactory,
+    this.xmlBuilder,
+  });
+
+  @override
+  String get id;
+
+  /// Returns true if the ID of this object is equal to the default ID value defined in [TSDefaults].
+  @override
+  bool get isDefault => id == TSDefaults.defaultIdValue;
+
   /// Converts this object to a YAML string.
   ///
   /// Uses [yamlBuilder] to serialize the result of [toJson()].
-  /// Throws an [UnimplementedError] if [yamlBuilder] is not provided.
-  String toYaml() {
-    if (yamlBuilder == null) throw UnimplementedError();
-    return yamlBuilder!(toJson());
-  }
+  String toYaml({
+    bool includeMetaData = true,
+    KeyValueBuilderDef? keyBuilder,
+    String metaDataKey = TSDefaults.metaDataKey,
+    KeyValueBuilderDef? listItemBuilder,
+  }) =>
+      yamlBuilder?.call(this, true) ??
+          ((writeable) => toJson().toYaml(
+            includeMetaData: includeMetaData,
+            metaDataKey: metaDataKey,
+            keyBuilder: keyBuilder,
+            listItemBuilder: listItemBuilder,
+          ))(this);
 
   /// Returns a function that builds a YAML string from a JSON map.
   ///
   /// Subclasses should override this getter to supply their YAML builder, or
   /// set it externally, if applicable. If not provided, [toYaml()] will throw.
-  String Function(Map<String, dynamic> json)? get yamlBuilder;
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  final String Function(TWriteableId writeable, bool includeMetaData)?
+  yamlBuilder;
 
   /// Converts this object to a Markdown string.
   ///
-  /// Uses [markdownBuilder] to serialize the result of [toJson()].
-  /// Throws an [UnimplementedError] if [markdownBuilder] is not provided.
-  String toMarkdown() {
-    if (markdownBuilder == null) throw UnimplementedError();
-    return markdownBuilder!(toJson());
+  /// Uses [mdFactory] to serialize the result of [toJson()].
+  String toMd({
+    TMdFactory? mdFactory,
+    bool includeMetaData = true,
+    int headingLevel = 1,
+    KeyValueBuilderDef? titleBuilder,
+    KeyValueBuilderDef? listItemBuilder,
+    String metaDataKey = TSDefaults.metaDataKey,
+    String emojiKey = TSDefaults.emojiKey,
+    String nameKey = TSDefaults.nameKey,
+    String descriptionKey = TSDefaults.descriptionKey,
+    String valueKey = TSDefaults.valueKey,
+    String valuesKey = TSDefaults.valuesKey,
+    String itemsKey = TSDefaults.itemsKey,
+  }) {
+    final pMdFactory = mdFactory ?? this.mdFactory;
+    if (pMdFactory == null) {
+      return toJson().toMd(
+        metaDataToFrontMatter: includeMetaData,
+        headingLevel: headingLevel,
+        titleBuilder: titleBuilder,
+        listItemBuilder: listItemBuilder,
+        frontMatterKey: metaDataKey,
+        emojiKey: emojiKey,
+        nameKey: nameKey,
+        descriptionKey: descriptionKey,
+        valueKey: valueKey,
+        valuesKey: valuesKey,
+        itemsKey: itemsKey,
+      );
+    }
+    return includeMetaData ? pMdFactory.build() : pMdFactory.buildBody();
   }
 
   /// Returns a function that builds a Markdown string from a JSON map.
   ///
   /// Subclasses should override this getter to supply their Markdown builder, or
-  /// set it externally, if applicable. If not provided, [toMarkdown()] will throw.
-  String Function(Map<String, dynamic> json)? get markdownBuilder;
+  /// set it externally, if applicable.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  final TMdFactory? mdFactory;
 
   /// Converts this object to an XML string.
   ///
   /// Uses [xmlBuilder] to serialize the result of [toJson()].
-  /// Throws an [UnimplementedError] if [xmlBuilder] is not provided.
-  String toXml() {
-    if (xmlBuilder == null) throw UnimplementedError();
-    return xmlBuilder!(toJson());
-  }
+  String toXml({
+    bool includeMetaData = true,
+    KeyValueBuilderDef? keyBuilder,
+    KeyValueBuilderDef? listItemBuilder,
+    String metaDataKey = TSDefaults.metaDataKey,
+  }) =>
+      xmlBuilder?.call(this, includeMetaData) ??
+          ((writeable) => toJson().toXml(
+            includeMetaData: includeMetaData,
+            metaDataKey: metaDataKey,
+            keyBuilder: keyBuilder,
+            listItemBuilder: listItemBuilder,
+          ))(this);
 
   /// Returns a function that builds an XML string from a JSON map.
   ///
   /// Subclasses should override this getter to supply their XML builder, or
   /// set it externally, if applicable. If not provided, [toXml()] will throw.
-  String Function(Map<String, dynamic> json)? get xmlBuilder;
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  final String Function(TWriteableId writeable, bool includeMeta)? xmlBuilder;
 }
